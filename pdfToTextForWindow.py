31c02e63695dd53a341f36f5e0f7a68b8f51e43d
homePC <limetree.indigo@gmail.com>
2020-04-02 08:46:08 +0900

    다트 크롤링 내용 추가

--- pdfToTextForWindow.py
+++ pdfToTextForWindow.py
@@ -1,7 +1,7 @@
 # jdk설치, 직접 다운받아서 해당폴더에 복사해야지 error없이 수행
 from tika import parser
 import os
-import time
+
 
 inputPath = "C:\\Users\\UpC\\Desktop\\crawlingTest\\"
 outputPath = "C:\\Users\\UpC\\Desktop\\crawlingTestTxt\\"
